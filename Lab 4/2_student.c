d9cddaf5d321ed7e023c0f94a9bba405d4d837ea
Aditya Pankaj Mathpal <148540279+aditya-mathpal@users.noreply.github.com>
2024-08-11 01:24:46 +0530

    Update 2_student.c

--- Lab 4/2_student.c
+++ Lab 4/2_student.c
@@ -57,4 +57,23 @@ int main() {
 	free(s);
 	return 0;
 }
-
+/*
+output:
+enter the number of students: 3
+enter details for student 1:
+enter name: A
+enter roll number: 7
+enter CGPA: 8.1
+enter details for student 2:
+enter name: B
+enter roll number: 2
+enter CGPA: 8.5
+enter details for student 3:
+enter name: C
+enter roll number: 4
+enter CGPA: 9.2
+all students roll number-wise:
+Student B, roll number 2, has a CGPA of 8.50
+Student C, roll number 4, has a CGPA of 9.20
+Student A, roll number 7, has a CGPA of 8.10
+*/
