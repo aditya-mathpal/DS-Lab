6b3790792d9dee633c55f81ed8454b50688934dd
Aditya Mathpal <adimathpal@gmail.com>
2024-09-06 21:14:10 +0530

    Fixed Q2

--- Lab 2/2_palindrome.c
+++ Lab 2/2_palindrome.c
@@ -1,21 +1,22 @@
 #include<stdio.h>
 #include<string.h>
 
-int Pal(char *s, int n) {
-	int i;
-	for(i=0;i<n/2;i++)
-		if(*(s+i) != *(s+n-1-i)) return 0;
-	return 1;
+int Pal(char *s, int n, int *i) {
+	if(*i == n/2) return 1;
+	if(*(s+*i) != *(s+n-1-*i)) return 0;
+	(*i)++;
+	return Pal(s,n,i);
 }
 
 int main() {
+	int *i;
+	*i = 0;
 	char s[100];
 	printf("enter a string: ");
 	fgets(s,100,stdin);
 	s[strcspn(s,"\n")] = 0;
-	int len;
-	for(len=0;s[len]!='\0';len++);
-	if(Pal(s,len)) printf("palindrome\n");
+	int len=strlen(s);
+	if(Pal(s,len,i)) printf("palindrome\n");
 	else printf("not palindrome\n");
 	return 0;
 }
